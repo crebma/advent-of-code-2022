3b8e7531df7c303ad8f3748a594c5cd15b8dfb77
Amber Conville <crebma@gmail.com>
2022-12-06 16:55:22 -0500

    Refactor RoundScorer to use a map

--- src/main/kotlin/rockPaperScissors/RoundScorer.kt
+++ src/main/kotlin/rockPaperScissors/RoundScorer.kt
@@ -4,29 +4,22 @@ import rockPaperScissors.Move.*
 import rockPaperScissors.RoundOutcome.*
 
 class RoundScorer {
+    private val winWith = mapOf(
+        Rock to Paper, Paper to Scissors, Scissors to Rock
+    )
+    private val loseWith = mapOf(
+        Rock to Scissors, Paper to Rock, Scissors to Paper
+    )
+
     fun score(round: String): Int {
         val plays = round.split(" ")
         val theirMove = Move.fromLetter(plays[0])
         val roundOutcome = RoundOutcome.fromLetter(plays[1])
         val myMove = when (roundOutcome) {
-            Draw -> {
-                theirMove
-            }
-            Win -> {
-                when(theirMove) {
-                    Rock -> Paper
-                    Paper -> Scissors
-                    Scissors -> Rock
-                }
-            }
-            else -> {
-                when(theirMove) {
-                    Rock -> Scissors
-                    Paper -> Rock
-                    Scissors -> Paper
-                }
-            }
+            Draw -> theirMove
+            Win -> winWith[theirMove]
+            else -> loseWith[theirMove]
         }
-        return myMove.value + roundOutcome.value
+        return myMove!!.value + roundOutcome.value
     }
 }
